2922f9c77c3b4ab16e687e95605394f71d89ef58
Andre Igor <andreigormenezes@gmail.com>
2022-04-07 16:35:19 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,52 @@ function calcularMDC() {
 
     mdc = a
 
-    const result = document.getElementById('resultText')
+    const result = document.getElementById('resultTextMDC')
 
     result.innerHTML = `O resultado é: ${mdc}`
 
+}
+
+function calcularMMC() {
+    const input3 = document.getElementById('input3')
+    const input4 = document.getElementById('input4')  
+
+    var a, b, divisor, mmc, maior;
+
+    a = input3.value
+    b = input4.value
+
+    if (a > b) {
+        maior = a
+    } else {
+        maior = b
+    }
+
+    divisor = 2;
+    mmc = 1;
+    while (divisor <= maior) {
+        if (a % divisor === 0 || b % divisor === 0) {
+            if (a % divisor === 0 && b % divisor === 0) {
+            a = a / divisor;
+            b = b / divisor;
+            mmc = mmc * divisor;
+            }
+
+            if (a % divisor === 0 && b % divisor !== 0) {
+            a = a / divisor;
+            mmc = mmc * divisor;
+            }
+
+            if (b % divisor === 0 && a % divisor !== 0) {
+            b = b / divisor;
+            mmc = mmc * divisor;
+            }
+        } else {
+            divisor = divisor + 1;
+        }
+    }
+
+    const result = document.getElementById('resultTextMMC')
+    result.innerHTML = `O resultado é ${mmc}`
 
 }
